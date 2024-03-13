4a19ac3cfcc7c1deda78060555929b09e9012ad6
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-13 09:39:45 -0500

    PYTHON\200 TOPICS\05_Variables in Python SUCCESS

---
+++ 200 TOPICS/05_Variables in Python/05_Variables_Python.py
@@ -0,0 +1,15 @@
+# Variable declaration and assignment
+my_variable = 42
+another_variable = "Hello, world!"
+
+# Printing the variables
+print("my_variable:", my_variable)
+print("another_variable:", another_variable)
+
+# Variable reassignment
+my_variable = 100
+another_variable = "Goodbye!"
+
+# Printing the updated variables
+print("my_variable (updated):", my_variable)
+print("another_variable (updated):", another_variable)
