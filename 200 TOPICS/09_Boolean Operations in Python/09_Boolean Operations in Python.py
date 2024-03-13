ac4a1027a752e058fd2e4c161bb00fba39ea1d3e
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-13 09:57:40 -0500

    PYTHON\200 TOPICS\09_Boolean Operations in Python

---
+++ 200 TOPICS/09_Boolean Operations in Python/09_Boolean Operations in Python.py
@@ -0,0 +1,45 @@
+# Boolean variables
+x = True
+y = False
+
+# Logical AND
+result_and = x and y
+print("Logical AND:", result_and)
+
+# Logical OR
+result_or = x or y
+print("Logical OR:", result_or)
+
+# Logical NOT
+result_not_x = not x
+result_not_y = not y
+print("Logical NOT (x):", result_not_x)
+print("Logical NOT (y):", result_not_y)
+
+# Comparison operators
+a = 10
+b = 20
+
+# Equality
+result_equal = a == b
+print("Equality:", result_equal)
+
+# Inequality
+result_not_equal = a != b
+print("Inequality:", result_not_equal)
+
+# Greater than
+result_greater_than = a > b
+print("Greater than:", result_greater_than)
+
+# Less than
+result_less_than = a < b
+print("Less than:", result_less_than)
+
+# Greater than or equal to
+result_greater_equal = a >= b
+print("Greater than or equal to:", result_greater_equal)
+
+# Less than or equal to
+result_less_equal = a <= b
+print("Less than or equal to:", result_less_equal)
