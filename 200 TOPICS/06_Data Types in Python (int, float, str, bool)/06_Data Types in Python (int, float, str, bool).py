1b6e6595bbe7c74dd47e2e57f36114399c2edb6c
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-13 09:40:56 -0500

    PYTHON\200 TOPICS\05_Variables in Python

---
+++ 200 TOPICS/06_Data Types in Python (int, float, str, bool)/06_Data Types in Python (int, float, str, bool).py
@@ -0,0 +1,25 @@
+# Integer
+my_integer = 42
+print("Integer:", my_integer)
+
+# Float
+my_float = 3.14
+print("Float:", my_float)
+
+# String
+my_string = "Hello, world!"
+print("String:", my_string)
+
+# Boolean
+my_boolean = True
+print("Boolean:", my_boolean)
+
+# Operations with different data types
+result_addition = my_integer + my_float
+print("Addition result:", result_addition)
+
+result_concatenation = my_string + " " + str(my_integer)
+print("String concatenation result:", result_concatenation)
+
+result_comparison = my_float > my_integer
+print("Comparison result:", result_comparison)
