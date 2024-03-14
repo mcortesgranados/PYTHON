acd951c3b72b84f231641671422320901822464f
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-14 02:28:28 -0500

    PYTHON\200 TOPICS\11_Type Conversion in Python

---
+++ 200 TOPICS/11_Type Conversion in Python/11_Type Conversion in Python.py
@@ -0,0 +1,34 @@
+# Integer to String
+integer_number = 42
+string_number = str(integer_number)
+print("Integer to String:", string_number)
+
+# String to Integer
+string_number = "123"
+integer_number = int(string_number)
+print("String to Integer:", integer_number)
+
+# Float to Integer
+float_number = 3.14
+integer_number = int(float_number)
+print("Float to Integer:", integer_number)
+
+# Integer to Float
+integer_number = 42
+float_number = float(integer_number)
+print("Integer to Float:", float_number)
+
+# String to Float
+string_number = "3.14"
+float_number = float(string_number)
+print("String to Float:", float_number)
+
+# Boolean to String
+boolean_value = True
+string_boolean = str(boolean_value)
+print("Boolean to String:", string_boolean)
+
+# String to Boolean
+string_boolean = "True"
+boolean_value = bool(string_boolean)
+print("String to Boolean:", boolean_value)
