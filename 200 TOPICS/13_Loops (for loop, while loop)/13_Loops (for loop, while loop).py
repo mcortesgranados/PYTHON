9b43bfe884247291ea2a041a0d96c817b7ced35e
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-14 02:29:28 -0500

    PYTHON\200 TOPICS\13_Loops (for loop, while loop)

---
+++ 200 TOPICS/13_Loops (for loop, while loop)/13_Loops (for loop, while loop).py
@@ -0,0 +1,11 @@
+# Example 1: for loop
+print("Example 1: for loop")
+for i in range(5):
+    print("Iteration", i)
+
+# Example 2: while loop
+print("\nExample 2: while loop")
+count = 0
+while count < 5:
+    print("Iteration", count)
+    count += 1
