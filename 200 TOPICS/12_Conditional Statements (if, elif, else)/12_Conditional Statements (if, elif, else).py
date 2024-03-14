4906927b83cfdf39754d917226d2b52b90c7c31d
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-14 02:29:02 -0500

    PYTHON\200 TOPICS\12_Conditional Statements (if, elif, else)

---
+++ 200 TOPICS/12_Conditional Statements (if, elif, else)/12_Conditional Statements (if, elif, else).py
@@ -0,0 +1,10 @@
+# Example: Checking a person's age
+age = 25
+
+# Conditional statement to check age
+if age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
