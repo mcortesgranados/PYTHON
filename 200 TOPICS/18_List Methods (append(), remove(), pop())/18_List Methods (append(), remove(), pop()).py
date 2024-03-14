236cc021cc4eb4c025fee0daa4e5daf162cbc4b2
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-14 02:36:10 -0500

    PYTHON\200 TOPICS\18_List Methods (append(), remove(), pop())

---
+++ 200 TOPICS/18_List Methods (append(), remove(), pop())/18_List Methods (append(), remove(), pop()).py
@@ -0,0 +1,16 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("Original list:", my_list)
+
+# append() method
+my_list.append(6)
+print("List after appending 6:", my_list)
+
+# remove() method
+my_list.remove(3)
+print("List after removing 3:", my_list)
+
+# pop() method
+popped_element = my_list.pop()
+print("Popped element:", popped_element)
+print("List after popping:", my_list)
